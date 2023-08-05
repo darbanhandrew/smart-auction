d2ebe3320719a7ce3d20f037be4d87adc5e3fb32
Mohammad <darbanhandrew@gmail.com>
2023-08-05 21:38:17 +0330

    thanks

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -1,6 +1,6 @@
 import { ThemedLayoutV2, ThemedTitleV2 } from "@refinedev/antd";
 import { Outlet } from "@remix-run/react";
-
+import { useEffect } from "react";
 import type { LoaderArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { authProvider } from "~/authProvider";
@@ -8,9 +8,40 @@ import { authProvider } from "~/authProvider";
 import { AppIcon } from "@components/app-icon";
 import { Header } from "@components/header";
 import { account, functions } from "~/utility";
+import { use } from "i18next";
 export default function BaseLayout() {
   //render menu using meta data label as title and name as resource name
+  useEffect(() => {
+    const getUserMemberships = async () => {
+      const user = await account.get();
+
+      if (user) {
+        const result = await functions.createExecution("userFunctions", JSON.stringify({
+          type: "listMemberships",
+          body: {
+            user_id: user.$id
+          }
+        }));
+
+        const responseObj = JSON.parse(result.response);
+        let isAdmin = false;
+
+        responseObj.memberships.forEach((membership: any) => {
+          if (membership.roles.includes("admin")) {
+            isAdmin = true;
+          }
+        });
+
+        if (!isAdmin) {
+          window.location.href = "https://desk.smartauctionhouse.com";
+        }
+      }
+    };
+
+    getUserMemberships();
+  }, []);
   return (
+
     <>
       <ThemedLayoutV2
         Header={Header}
@@ -42,36 +73,5 @@ export async function loader({ request }: LoaderArgs) {
 
     throw redirect(redirectTo ?? "/login");
   }
-  else {
-    const user = await account.get();
-
-
-    if (user) {
-      const result = await functions.createExecution("userFunctions", JSON.stringify({
-        type: "listMemberships",
-        body: {
-          //generate
-          user_id: user.$id
-        }
-      }));
-      /*result is like this: 
-      {"total":1,"memberships":[{"$id":"64bc5aac02ba4666f28e","$createdAt":"2023-07-22T22:39:40.011+00:00","$updatedAt":"2023-07-22T22:39:40.011+00:00","userId":"64ae415f66d0c9fda80b","userName":"محمد دربان باران","userEmail":"+989981662304@sah.com","teamId":"64bc5a64bfd9bd75c9bc","teamName":"admin","invited":"2023-07-22T22:39:40.011+00:00","joined":"2023-07-22T22:39:40.011+00:00","confirm":true,"roles":["admin"]}]}*/
-      //get the role of the user from all memberships and if any of them is not admin redirect user to https://desk.smartauctionhouse.com
-      let isAdmin = false;
-      const responseObj = JSON.parse(result.response);
-      responseObj.memberships.forEach((membership: any) => {
-        if (membership.roles.includes("admin")) {
-          isAdmin = true;
-        }
-      }
-      );
-
-      if (!isAdmin) {
-        throw redirect("https://desk.smartauctionhouse.com");
-      }
-    }
-
-  }
-
   return {};
 }
