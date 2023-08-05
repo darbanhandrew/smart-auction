99a9462c9e52961465b49b9db0431a992eaae40c
Mohammad <darbanhandrew@gmail.com>
2023-08-05 15:17:03 +0330

    changed cookies on root level

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -37,36 +37,40 @@ export default function BaseLayout() {
 export async function loader({ request }: LoaderArgs) {
   const { authenticated, redirectTo } = await authProvider.check(request);
   //check if authProvider.getIdentity() is not returning null and create a user object from it
-  const user = await account.get();
-
-
-  if (user) {
-    const result = await functions.createExecution("userFunctions", JSON.stringify({
-      type: "listMemberships",
-      body: {
-        //generate
-        userId: user ? user.$id : null
-      }
-    }));
-    /*result is like this: 
-    {"total":1,"memberships":[{"$id":"64bc5aac02ba4666f28e","$createdAt":"2023-07-22T22:39:40.011+00:00","$updatedAt":"2023-07-22T22:39:40.011+00:00","userId":"64ae415f66d0c9fda80b","userName":"محمد دربان باران","userEmail":"+989981662304@sah.com","teamId":"64bc5a64bfd9bd75c9bc","teamName":"admin","invited":"2023-07-22T22:39:40.011+00:00","joined":"2023-07-22T22:39:40.011+00:00","confirm":true,"roles":["admin"]}]}*/
-    //get the role of the user from all memberships and if any of them is not admin redirect user to https://desk.smartauctionhouse.com
-    let isAdmin = false;
-    const responseObj = JSON.parse(result.response);
-    responseObj.memberships.forEach((membership: any) => {
-      if (membership.roles.includes("admin")) {
-        isAdmin = true;
-      }
-    }
-    );
-
-    if (!isAdmin) {
-      throw redirect("https://desk.smartauctionhouse.com");
-    }
-  }
 
   if (!authenticated) {
+
     throw redirect(redirectTo ?? "/login");
+  }
+  else {
+    const user = await account.get();
+
+
+    if (user) {
+      const result = await functions.createExecution("userFunctions", JSON.stringify({
+        type: "listMemberships",
+        body: {
+          //generate
+          userId: user ? user.$id : null
+        }
+      }));
+      /*result is like this: 
+      {"total":1,"memberships":[{"$id":"64bc5aac02ba4666f28e","$createdAt":"2023-07-22T22:39:40.011+00:00","$updatedAt":"2023-07-22T22:39:40.011+00:00","userId":"64ae415f66d0c9fda80b","userName":"محمد دربان باران","userEmail":"+989981662304@sah.com","teamId":"64bc5a64bfd9bd75c9bc","teamName":"admin","invited":"2023-07-22T22:39:40.011+00:00","joined":"2023-07-22T22:39:40.011+00:00","confirm":true,"roles":["admin"]}]}*/
+      //get the role of the user from all memberships and if any of them is not admin redirect user to https://desk.smartauctionhouse.com
+      let isAdmin = false;
+      const responseObj = JSON.parse(result.response);
+      responseObj.memberships.forEach((membership: any) => {
+        if (membership.roles.includes("admin")) {
+          isAdmin = true;
+        }
+      }
+      );
+
+      if (!isAdmin) {
+        throw redirect("https://desk.smartauctionhouse.com");
+      }
+    }
+
   }
 
   return {};
